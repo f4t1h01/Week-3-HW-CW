937159756bcab98d64fcb8a2e2cc5fbdd806d9d8
f4t1h01 <145100532+f4t1h01@users.noreply.github.com>
2023-10-04 11:00:16 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,99 @@
+//
+//  main.cpp
+//  LABASSIGN
+//
+//  Created by Mahamatfotix Shaxin on 04/10/23.
+//
+Problem 3
+//#include <iostream>
+//
+//using namespace std;
+//
+// int main()
+//
+//{
+//    bool one = true;
+//    bool zero = false;
+//     cout << "one" << " = true " <<  std::endl;
+//     cout << "zero" << " = false " <<  std::endl;
+//
+//
+//}
+
+
+problem 5
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+   
+//    double doublenumber1 = 25e3;
+//    double doublenumber2 = 15e4;
+//
+//    float floatnumber1 = 25e3;
+//    float floatnumber2 = 15e4;
+//
+//    cout << "doublenumber1" << doublenumber1 << endl;
+//    cout << "doublenumber2" << doublenumber2 << endl;
+//
+//    cout << "floatnumber1" << floatnumber1 << endl;
+//    cout << "floatnumber2" << floatnumber2 << endl;
+    
+    
+ Problem 9
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//
+//    cout << "enter two numbers: ";
+//    int a, b;
+//    bool is_equal;
+//
+//    cin >> a;
+//    cin >> b;
+//
+//    is_equal = a==b;
+//
+//    cout << boolalpha << is_equal << endl;
+//
+//    return 0;
+//
+
+
+Problem 12
+//#include <iostream>
+//using namespace std;
+//int main() {
+//
+//    float x = 30*74.5/100;
+//    cout << x << endl;
+//    return 0;
+//
+//}
+
+ Problem 13
+//#include <iostream>
+//using namespace std;
+//int main() {
+//
+//    int cp = 4;
+//    float gpaForcp = 4.8;
+//
+//    int aa = 2;
+//    float gpaForaa = 2.5;
+//
+//    int ict = 3;
+//    float gpaForict= 3.5;
+//
+//    int cs = 4;
+//    float gpaForcs = 3.0;
+//
+//    float finalGpa = (cp * gpaForcp + aa * gpaForaa + ict * gpaForict + cs * gpaForcs) / (cp + aa + ict + cs);
+//
+//    cout << "Final GPA is" << finalGpa << endl;
+//
+//
+//}
